41415b76bbdeea1d2aaa6ac95d9e8a6cf8ea25a5
Naveezzz <naveen2n03@gmail.com>
2024-04-24 21:27:06 +0530

    mofified index.html

--- static/script/script.js
+++ static/script/script.js
@@ -1,31 +1,36 @@
-option = {
+var option = {
     title: [
       {
-        text: 'Radial Polar Bar Label Position (middle)'
-      }
+        text: "Radial Polar Bar Label Position (middle)",
+      },
     ],
     polar: {
-      radius: [30, '80%']
+      radius: [30, "80%"],
     },
     radiusAxis: {
-      max: 4
+      max: 4,
     },
     angleAxis: {
-      type: 'category',
-      data: ['a', 'b', 'c', 'd'],
-      startAngle: 75
+      type: "category",
+      data: ["a", "b", "c", "d"],
+      startAngle: 75,
     },
     tooltip: {},
     series: {
-      type: 'bar',
+      type: "bar",
       data: [2, 1.2, 2.4, 3.6],
-      coordinateSystem: 'polar',
+      coordinateSystem: "polar",
       label: {
         show: true,
-        position: 'middle',
-        formatter: '{b}: {c}'
-      }
+        position: "middle",
+        formatter: "{b}: {c}",
+      },
     },
-    animation: false
+    animation: false,
   };
-  
+
+  // Initialize ECharts instance
+  var myChart = echarts.init(document.getElementById("chart"));
+
+  // Set chart options and render
+  myChart.setOption(option);
